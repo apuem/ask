b47bc03ca820e3fcf2d97858516166d76ff7070b
jk10274 <72466440+jk10274@users.noreply.github.com>
2021-07-10 08:47:28 +0200

    add loadContent()

--- main.js
+++ main.js
@@ -1,43 +1,77 @@
+var question;
 var questionId;
 var questionCategory;
 var result;
 var logged;
 var cookiesAccepted;
+var questionById = false;
+
+getCookies();
+
+if (cookiesAccepted == !true) {
+    cookiesAccepted = confirm("Agree our cookies?");
+    setCookies(questionId, questionCategory, result, logged, cookiesAccepted);
+}
+
+if (questionCategory == "undefined") {
+    questionCategory = "general";
+}
 
 const params = new URLSearchParams(window.location.search);
 if (params.has("id") == true) {
     id = params.get("id");
-    loadContent();
+    questionById = true;
 }
 if (params.has("logged") == true) {
     logged = params.get("logged");
-    loadContent();
 }
 if (params.has("category") == true) {
     questionCategory = params.get("category");
-    loadContent(questionId, );
 }
+
+setCookies(questionId, questionCategory, result, logged, cookiesAccepted);
+
+loadQuestion();
 
 function loadQuestion() {
-    setCookies(questionId, questionCategory, result, logged, cookiesAccepted);
+
+    if (questionId == null) {
+        nextQuestion();
+    }
+    if (questionById == false) {
+        question = httpGet("https://ask-api.vercel.app/api/getQuestionByCategory/" + questionCategory);
+    } else {
+        question = httpGet("https://ask-api.vercel.app/api/getQuestionById/" + questionId);
+    }
+
+    var q = JSON.parse(question);
+
+    setInput(q.question, q.id, q.answer1, q.answer2, q.answer3, q.answer4);
 }
 
-if (idParam != null) {
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+/* if (idParam != null) {
     id = idParam;
     getQuestionbyId(id);
 } else {
     getQuestionbyCategory(categoryParam)
-}
+} */
 
 function setCookies(questionId, questionCategory, result, logged, cookiesAccepted) {
     Cookies.set('questionId', questionId);
     Cookies.set('questionCategory', questionCategory);
     Cookies.set('result', result);
     Cookies.set('logged', logged);
-    Cookies.set('cookiesAccepted', logged);
+    Cookies.set('cookiesAccepted', cookiesAccepted);
 }
 
-function getCookies(questionId, questionCategory, result, logged, cookiesAccepted) {
+function getCookies() {
     questionId = Cookies.get('questionId');
     questionCategory = Cookies.get('questionCategory');
     result = Cookies.get('result');
